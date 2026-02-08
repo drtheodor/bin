d09fbc46f1e878afeaf9e1e980d578d5f6bc6832
Theo <git@theo.is-a.dev>
2026-02-09 00:16:29 +0300

    Update Vite root and output directory paths

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import autoprefixer from 'autoprefixer';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	root: 'src',
+	css: {
+		postcss: {
+			plugins: [autoprefixer]
+		}
+	},
+	build: {
+		outDir: fileURLToPath(new URL('dist/', import.meta.url)),
+		chunkSizeWarningLimit: 1_000, // 1 MB
+		emptyOutDir: true,
+		rollupOptions: {
+			output: {
+				entryFileNames: `assets/[name].js`,
+				chunkFileNames: `assets/[name].js`,
+				assetFileNames: `assets/[name].[ext]`
+			}
+		}
+	}
+});
